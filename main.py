d15ad19b15358c7c219feba8e697a9dc1c29cf98
stone-04 <168131362+stone-04@users.noreply.github.com>
2024-05-14 11:27:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime
 app = Flask(__name__)
 app.secret_key = "supersecretkey"  # very secure
 
-
+ 
 @app.context_processor
 def utility_processor():
     def format_datetime(dt):
